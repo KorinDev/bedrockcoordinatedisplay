533dad99d9624a389cb65d662f4dd7273c1ec024
KorinDev <92170697+KorinDev@users.noreply.github.com>
2026-06-04 14:28:24 +0200

    Initial Commit

---
+++ src/client/java/net/korin/bedrock_coordinates_display/client/BedrockCoordinatesDisplayClient.java
@@ -0,0 +1,10 @@
+package net.korin.bedrock_coordinates_display.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BedrockCoordinatesDisplayClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
